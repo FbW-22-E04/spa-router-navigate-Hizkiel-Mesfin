e1a5453467e83453f84a094f5c24ea8875ba8ad2
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-10-19 14:53:00 +0200

    done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+  const nav = useNavigate();
+  return (
+    <div>
+      <h1>Contact</h1>
+      <button onClick={() => nav("/home")}>Submit</button>
+    </div>
+  );
 };
 
 export default Contact;
